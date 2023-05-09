83fc977369b493cd5c6a0af101fb24e367974f95
Sönke Hahn <soenkehahn@gmail.com>
2023-05-08 20:23:57 -0400

    Add initial example

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  outputs = { self, nixpkgs }:
+    let pkgs = (import nixpkgs { system = "x86_64-linux"; });
+    in
+    with pkgs; {
+      packages.x86_64-linux.default = hello;
+      checks.x86_64-linux = {
+        without-hashbang = runCommand "without-hashbang"
+          { buildInputs = [ just ]; }
+          ''
+            touch $out
+            cd ${self}
+            just without-hashbang
+          '';
+        with-hashbang = runCommand "with-hashbang"
+          { buildInputs = [ just ]; }
+          ''
+            touch $out
+            cd ${self}
+            just with-hashbang
+          '';
+      };
+    };
+}
---
+++ justfile
@@ -0,0 +1,6 @@
+with-hashbang:
+  #!/usr/bin/env bash
+  echo just recipe with hashbang
+
+without-hashbang:
+  echo just recipe without hashbang
